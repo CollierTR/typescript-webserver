0ae2c408058c02bcf561a53079d9d70691beeec6
CollierTR <tristancollier777@gmail.com>
2026-06-14 22:16:17 -0500

    add user metric logging middlewear

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { Request, Response } from "express";
 import path from "path";
-import { logResponses } from "./middleware.js";
+import { logResponses, userMetrics } from "./middleware.js";
+import { config } from "./config.js";
 
 const PORT = 8080;
 
@@ -9,20 +10,43 @@ const app = express();
 
 app.use(logResponses);
 
-app.get("/healthz", (req, res) => {
+app.get("/api/healthz", (req, res) => {
   console.log("Server is ready!");
   res.set("Content-Type", "text/plain; charset=utf-8").status(200).send("OK");
 });
 
-app.use("/app", express.static(path.join(import.meta.dirname, "..", "src", "app")));
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
-}).on("error", (err: NodeJS.ErrnoException) => {
-  if (err.code === "EADDRINUSE") {
-    console.error(`Port ${PORT} is already in use`);
-  } else {
-    console.error("Server error:", err);
-  }
-  process.exit(1);
+app.get("/api/metrics", (req, res) => {
+  console.log(`Hits: ${config.fileserverHits}`);
+  res
+    .set("Content-Type", "text/plain; charset=utf-8")
+    .status(200)
+    .send(`Hits: ${config.fileserverHits}`);
 });
+
+app.get("/api/reset", (req, res) => {
+  console.log(`Resetting user metrics count...`);
+  config.fileserverHits = 0;
+  res
+    .set("Content-Type", "text/plain; charset=utf-8")
+    .status(200)
+    .send(`Reset user metric count`);
+});
+
+app.use(
+  "/app",
+  userMetrics,
+  express.static(path.join(import.meta.dirname, "..", "src", "app")),
+);
+
+app
+  .listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`);
+  })
+  .on("error", (err: NodeJS.ErrnoException) => {
+    if (err.code === "EADDRINUSE") {
+      console.error(`Port ${PORT} is already in use`);
+    } else {
+      console.error("Server error:", err);
+    }
+    process.exit(1);
+  });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import { config } from "./config.js";
 
 type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+export const userMetrics: Middleware = (req, res, next) => {
+  config.fileserverHits++;
+  next();
+};
 
 export const logResponses: Middleware = (req, res, next) => {
   res.on("finish", () => {
